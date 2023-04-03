d899095bf240c07e16806c7e6ca912a499b48788
Seonp_Na <seono5670@gmail.com>
2023-04-03 16:18:45 +0900

    JS: Quotes auto change

--- js/quotes.js
+++ js/quotes.js
@@ -50,8 +50,10 @@ const quotes = [
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
+setInterval(() => {
 
-const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  const todayQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-quote.innerText = todayQuote.quote;
-author.innerText = ` - ${todayQuote.author}`;
+  quote.innerText = todayQuote.quote;
+  author.innerText = ` - ${todayQuote.author}`;
+}, 15000);
