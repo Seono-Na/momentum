9fe91d02b42aea23bfd60af38e398a034b01de80
Seono-Na <seono5670@gmail.com>
2023-04-04 00:47:54 +0900

    JS: Mini clock

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,25 @@
 "use strict";
 
 const clock = document.querySelector("p.clock");
+const miniClock = document.querySelector("p.clock.mini");
 
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  let ampmHours = hours;
+  let ampm = "";
+  if (hours > 12) {
+    ampmHours = String(parseInt(ampmHours) - 12);
+    ampm = "PM";
+  } else {
+    ampm = "AM";
+  }
+
   clock.innerText = `${hours}:${minutes}:${seconds}`;
+  miniClock.innerText = `${ampm} ${ampmHours}:${minutes}:${seconds}`;
 }
 
 getClock();
