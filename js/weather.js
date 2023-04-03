f4ba6f7046030ea20105a91c256540e76090b3ef
Seonp_Na <seono5670@gmail.com>
2023-04-03 12:43:33 +0900

    JS: Weather

--- js/weather.js
+++ js/weather.js
@@ -14,7 +14,7 @@ function onGeoOK(position) {
       console.log(data.name);
       console.log(typeof data.name);
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${Math.round(data.main.temp)}˚`;
     });
 }
 
