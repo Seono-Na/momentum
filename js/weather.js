bd2f3425092a5e0c23b269a2520ba4a6ff3a936f
Seonp_Na <seono5670@gmail.com>
2023-04-04 14:53:31 +0900

    JS: Weather edit

--- js/weather.js
+++ js/weather.js
@@ -4,20 +4,15 @@ const AK = "0dbc920b61c5857163244bc5a0c06222";
 function onGeoOK(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log("You live in", lat, lon);
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${AK}&lang=kr&units=metric`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
-      console.log(data.name);
-      console.log(typeof data.name);
-      city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${Math.round(
-        data.main.temp
-      )}˚`;
-    });
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${AK}&lang=kr&units=metric`
+  fetch(url).then(response => response.json()).then(data => {
+    const weather = document.querySelector("#weather span:first-child")
+    const city = document.querySelector("#weather span:last-child")
+    console.log(data.name);
+    console.log(typeof data.name);
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${Math.round(data.main.temp)}˚`;
+  });
 }
 
 function onGeoError() {
