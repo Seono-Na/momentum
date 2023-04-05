9e81e322634f99c44a86dd7526dfc754c3492111
Seonp_Na <seono5670@gmail.com>
2023-04-05 14:48:40 +0900

    JS: Icon-description

--- js/linkicon.js
+++ js/linkicon.js
@@ -3,10 +3,14 @@ const iconHoverClass = "mouseover-icon";
 
 function mouseenterIcon() {
   this.classList.add(iconHoverClass);
+  const iconDescription = this.querySelectorAll("div")
+  iconDescription[0].style.display = "flex"
 }
 
 function mouseleaveIcon() {
   this.classList.remove(iconHoverClass);
+  const iconDescription = this.querySelectorAll("div")
+  iconDescription[0].style.display = "none"
 }
 
 iconWraps.forEach((iconWrap) => {
